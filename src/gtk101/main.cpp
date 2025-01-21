a52dd754cc4ca94e5155354301950a7e4e34f00e
mahaidong <mahaidong@caad.xyz>
2025-01-21 17:24:32 +0800

    gtk_4_16

--- src/gtk101/main.cpp
+++ src/gtk101/main.cpp
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <glib/gtypes.h>
 
 static void
 activate (GtkApplication* app,
@@ -17,7 +18,7 @@ main (int argc, char **argv)
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
